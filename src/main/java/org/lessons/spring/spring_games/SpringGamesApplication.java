841824bfff247a86e10c4261d636e800634db9b7
alessandroschiano98 <schianoski98@gmail.com>
2025-06-24 21:12:16 +0200

    first commit

---
+++ src/main/java/org/lessons/spring/spring_games/SpringGamesApplication.java
@@ -0,0 +1,13 @@
+package org.lessons.spring.spring_games;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/lessons/spring/spring_games/SpringGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lessons.spring.spring_games;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
