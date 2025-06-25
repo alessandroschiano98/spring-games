4819e049e3309613a48862322f993539247f0516
alessandroschiano98 <schianoski98@gmail.com>
2025-06-25 21:34:29 +0200

    aggiunta pagina Show(dettagli giochi) con funzionamento button

--- src/main/java/org/lessons/spring/spring_games/controller/GameController.java
+++ src/main/java/org/lessons/spring/spring_games/controller/GameController.java
@@ -1,10 +1,12 @@
 package org.lessons.spring.spring_games.controller;
 
+import org.lessons.spring.spring_games.model.Game;
 import org.lessons.spring.spring_games.repository.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -18,6 +20,13 @@ public class GameController {
     public String index(Model model) {
         model.addAttribute("games", gameRepository.findAll());
         return "games/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable(name = "id") Integer id, Model model) {
+        Game game = gameRepository.findById(id).get();
+        model.addAttribute("game", game);
+        return "games/show";
     }
 
 }
