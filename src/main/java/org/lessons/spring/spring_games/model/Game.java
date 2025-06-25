92c6b5e470b096464aa1217f0e46d6b467531278
alessandroschiano98 <schianoski98@gmail.com>
2025-06-25 14:45:28 +0200

    aggiunta lista in ''games''

--- src/main/java/org/lessons/spring/spring_games/model/Game.java
+++ src/main/java/org/lessons/spring/spring_games/model/Game.java
@@ -1,5 +1,6 @@
 package org.lessons.spring.spring_games.model;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 
 import jakarta.persistence.Entity;
@@ -7,6 +8,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Lob;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -16,30 +18,41 @@ import jakarta.persistence.GenerationType;
 @Table(name = "games")
 public class Game {
 
+    // ! ID
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Size(min = 2, message = "Developer must containt at least 3 characters")
+    // ! TITLE 
+    @Size(min = 2, message = "Title must containt at least 3 characters")
     @NotBlank(message = "The title must not be blanks, empty or null")
     private String title;
 
+    // ! DEVELOPER
     @Size(min = 2, message = "Developer must containt at least 3 characters")
     @NotBlank(message = "The developer field  must not be blanks, empty or null")
     private String developer;
 
-    @Size(min = 2, message = "Description must contain at least 10 character")
+    // ! GENRE
+    @Size(min = 2, message = "Genre must contain at least 10 character")
     @NotBlank(message = "The genre field must not be blank, empty or null")
     private String genre;
 
+    // ! RELEASED-DATE
     @NotNull(message = "Release date must be filled")
     private LocalDate releaseDate;
 
+    // ! PRICE
+    @DecimalMin(value = "0.0", inclusive = false, message = "The price must be greater than 0.")
+    private BigDecimal price;
+
+    // ! DESCRIPTION
     @Lob
     @Size(min = 10, max = 2000, message = "Description must contain at least 10 character and not more than 2000")
     @NotBlank(message = "The developer field must not be blank, empty or null")
     private String description;
 
+    // ! IMG-URL
     @Lob
     @Size(min = 10, max = 1000, message = "Image url must containt at least 10 characters and not more than 1000")
     private String imageUrl;
@@ -100,6 +113,16 @@ public class Game {
     public void setImageUrl(String imageUrl) {
         this.imageUrl = imageUrl;
     }
+
+
+    public BigDecimal getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
 
     
 
